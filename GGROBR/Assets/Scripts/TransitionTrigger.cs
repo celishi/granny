4ff9491860c477281a505abd295b7c857d23f98d
celia <celiashi666@hotmail.com>
2021-12-29 23:08:30 -0500

    animation finally work

---
+++ GGROBR/Assets/Scripts/TransitionTrigger.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionTrigger : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1f;
+
+    private void Update()
+    {
+        if (GameObject.FindWithTag("grandma").GetComponent<grandma>().health == 0)
+        {
+            LoadNextScene();
+        }
+    }
+
+    public void LoadNextScene()
+    {
+        StartCoroutine(LoadLevel());
+    }
+
+    IEnumerator LoadLevel()
+    {
+        transition.SetTrigger("Start"); //play animation
+
+        yield return new WaitForSeconds(transitionTime); //wait
+
+        SceneManager.LoadScene("GameOver"); //load scene
+    }
+}
